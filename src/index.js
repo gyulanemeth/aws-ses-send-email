6a21b9dd76c395240015cc2828dbba069b80805b
AhmedQudaih <ahmedku0123@gmail.com>
2024-12-05 13:36:15 +0200

    add attachments

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { ValidationError, InternalServerError } from 'standard-api-errors'
     AWS_REGION
 */
 
-export default async ({ to, subject, html, from, replyTo, awsSecretAccessKey, awsAccessKeyId, region }) => {
+export default async ({ to, subject, html, from, replyTo, attachments, awsSecretAccessKey, awsAccessKeyId, region }) => {
   try {
     if (!to || !subject || !html) {
       throw new ValidationError('Missing params: to, subject and html are required.')
@@ -60,6 +60,7 @@ export default async ({ to, subject, html, from, replyTo, awsSecretAccessKey, aw
       subject,
       html,
       replyTo,
+      attachments,
       text: createTextVersion(html)
     })
     return {
--- src/index.test.js
+++ src/index.test.js
@@ -25,6 +25,17 @@ describe('Email testing', () => {
     expect(found).toBe('Email send successfully')
   })
 
+  test('success Send Email with Attachments', async () => {
+    const res = await sendEmail({ to: 'example@example.com', from: 'test@test.com', subject: 'Send Email TEST with Attachment', html: '<h1>Email sent successfully with attachment</h1>', attachments: [{ filename: 'test.txt', content: 'This is the content of the attachment.' }] })
+    expect(res.status).toBe(200)
+
+    const messageUrl = nodemailer.getTestMessageUrl(res.result.info)
+    const html = await fetch(messageUrl).then(response => response.text())
+    const regex = /Email sent successfully with attachment/g
+    const found = html.match(regex)[0]
+    expect(found).toBe('Email sent successfully with attachment')
+  })
+
   test('Send Email Validation error missing params', async () => {
     await expect(sendEmail({ subject: 'send Email TEST', html: '<h1>Should Not Be Sent </h1>' })).rejects.toThrowError('Missing params: to, subject and html are required.')
   })
